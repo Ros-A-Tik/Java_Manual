5320ebbc334e0d4c7fe4de093e00fe90e3c82e3a
Rostislav Terescenko <info@rosatik.com>
2025-03-17 19:29:36 +0200

    manipulatiig

--- src/TIME/Java_TIME.java
+++ src/TIME/Java_TIME.java
@@ -5,9 +5,9 @@ import java.time.*;
 public class Java_TIME {
 
     public static void main (String[] args) {
-        YearMonth may = YearMonth.of(2025, Month.MAY);
+        YearMonth may = YearMonth.of(2025, Month.FEBRUARY);
         may.atDay(1)
-                .datesUntil((may.atEndOfMonth()))
+                .datesUntil(may.atEndOfMonth())
                 .forEach(System.out::println);
     }
 }
