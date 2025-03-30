9c3dc68ce53c20fa49275af5bf130d46fba06154
Rostislav Terescenko <info@rosatik.com>
2025-03-30 18:26:32 +0300

    ghgh

---
+++ src/KOLEADYDAR/KoleadyDar_var.java
@@ -0,0 +1,60 @@
+package KOLEADYDAR;
+
+import java.util.ArrayList;
+
+public class KoleadyDar_var {
+
+    //Лето исчисление
+    public static String sutkiSvaroga = "Сутки Сварога";
+    public static String лкгпОшятшSutkiSvaroga = "Круг Жизни";
+    public static String letPeriodSutkiSvaroga = "Лето";
+    public static String meseaetLetoSutkiSvaroga = "Сороковник";
+
+    //Days of the week
+    public static String koleadyDar_Day_Penedelnik = "Понедельник";
+    public static String koleadyDar_Day_Vtornik = "Вторник";
+    public static String koleadyDar_Day_Treteinik = "Третейник";
+    public static String koleadyDar_Day_Chetverg = "Четверг";
+    public static String koleadyDar_Day_Peatnitsa = "Пятница";
+    public static String koleadyDar_Day_Shestitsa = "Шестица";
+    public static String koleadyDar_Day_Sedmitsa = "Седьмица";
+    public static String koleadyDar_Day_Osmotsa = "Осьмица";
+    public static String koleadyDar_Day_Nedelea = "Неделя";
+
+    //Время
+    public static String koleadyDar_Time_hour = "Час";
+    public static String koleadyDar_Time_chast = "Часть";
+    public static String koleadyDar_Time_dolea = "Доля";
+    public static String koleadyDar_Time_mgnovenie = "Мгновение";
+    public static String koleadyDar_Time_sig = "Сиг";
+    public static String koleadyDar_Time_santig = "Сантиг";
+
+    //Количество лет в сутках Сварога
+    public static int koleadyDar_SutkiSavaroga = 25920;
+    //Количество периодов в Сутках Сварога
+    public static int koleadyDar_PeriodSutkiSvaroga = 180;
+    //Количество лет в периоде
+    public static int koleadyDar_LetKrugJizni = 144;
+    //Количество сороковников в одном лете
+    public static int koleadyDar_SorokovnikLeto = 9;
+    //Количество дней в четном сороковнике
+    public static int koleadyDar_DaySorokovnik_0 = 40;
+    //Количество дней в нечетном сороковнике
+    public static int koleadyDar_DaySorokovnik_1 = 41;
+    //Количество часов в одном дне
+    public static int koleadyDar_hoursDay = 16;
+    //Количество частей в одном часе (9 x 16 часов)
+    public static int koleadyDar_chastHour = 144;
+    //Колличество долей в одной части (9 x 9 x 16)
+    public static int koleadyDar_doleaChast = 1296;
+    //Количество мгновений в одной доле (9 x 8)
+    public static int koleadyDar_gnoveniaDolea = 72;
+    //Количество сигов в оном мгновении
+    public static int koleadyDar_sigMgnovenie = 760;
+    //Количество сантигов в одном сиге
+    public static int koleadyDar_santifSig = 14000;
+
+
+
+
+}
