37e9f4c0328332f14c757f7c009c395489adb08d
Rostislav Sanduta <info@rosatik.com>
2025-04-17 10:30:07 +0300

    example 2

--- src/LAMBDA_EXPRESSIONS/Java_LAMBDA_Example_1.java
+++ src/LAMBDA_EXPRESSIONS/Java_LAMBDA_Example_1.java
@@ -15,8 +15,29 @@ public static void main (String [] args) {
 
     pw.println("A random value: " + myNum.getValue());
     pw.println("Another random value: " + myNum.getValue());
-    pw.println("One more random value: " +  myNum.getValue());
+    pw.println("One more random value: " + myNum.getValue());
 
+
+    Java_LAMBDA_Example_NumericTest numTest;
+    numTest = (n) -> (n % 2) == 0;
+
+    if (numTest.test(9)) {
+        pw.println("9 in not even");
+    }
+
+    if (numTest.test(10)) {
+        pw.println("10 in even.");
+    }
+
+    Java_LAMBDA_Example_NumericTest numNonNeg;
+    numNonNeg = (n) -> n >= 0;
+    if (numNonNeg.test(1)) {
+        pw.println("1 is non negative.");
+    }
+
+    if (numNonNeg.test(-1)) {
+        pw.println("-1 id negative");
+    }
 }
 
 }
---
+++ src/LAMBDA_EXPRESSIONS/Java_LAMBDA_Example_NumericTest.java
@@ -0,0 +1,6 @@
+package LAMBDA_EXPRESSIONS;
+
+public interface Java_LAMBDA_Example_NumericTest {
+
+    boolean test(int n);
+}
